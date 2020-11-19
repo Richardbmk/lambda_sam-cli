65178eacdf3270c7b7a3970bbc5572b2b872f9f5
Ricardo <rdobmk@gmail.com>
2020-11-19 16:39:04 +0100

    AWS SAM beggining

---
+++ add.js
@@ -0,0 +1,13 @@
+exports.handler = async (event) =>{
+    let {num1, num2} = JSON.parse(event.body);
+    console.log(JSON.parse(event.body));
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            num1: num1,
+            num2: num2,
+            result: num1 + num2
+        })
+    }
+}
